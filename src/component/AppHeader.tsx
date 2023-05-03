bd112c117b54054b26e6d19eec154ea759a8f5ee
KOlaGrek <kolagrek826@gmail.com>
2023-05-03 20:03:20 +0300

    LAB_3

---
+++ src/component/AppHeader.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import {Breadcrumb, Layout, Menu, theme} from 'antd';
+import Cars from "./Cars";
+import '../App.css'
+
+const {Header, Content, Footer, Sider} = Layout;
+
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+    (icon, index) => {
+        const key = String(index + 1);
+
+        return {
+            key: `sub${key}`,
+            icon: React.createElement(icon),
+            label: `subnav ${key}`,
+
+            children: new Array(4).fill(null).map((_, j) => {
+                const subKey = index * 4 + j + 1;
+                return {
+                    key: subKey,
+                    label: `option${subKey}`,
+                };
+            }),
+        };
+    },
+);
+
+const App: React.FC = () => {
+    const {
+        token: {colorBgContainer},
+    } = theme.useToken();
+
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo"/>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1}/>
+            </Header>
+            <Content style={{padding: '0 50px'}}>
+
+                <Layout style={{padding: '24px 0', background: colorBgContainer}}>
+                    {/*<Sider style={{ background: colorBgContainer }} width={200}>*/}
+                    {/*    <Menu*/}
+                    {/*        mode="inline"*/}
+                    {/*        defaultSelectedKeys={['1']}*/}
+                    {/*        defaultOpenKeys={['sub1']}*/}
+                    {/*        style={{ height: '100%' }}*/}
+                    {/*        items={items2}*/}
+                    {/*    />*/}
+                    {/*</Sider>*/}
+
+                    <div className='home-header'>
+                        Welcome
+                    </div>
+                    <Cars/>
+                </Layout>
+            </Content>
+        </Layout>
+    );
+};
+
+export default App;
