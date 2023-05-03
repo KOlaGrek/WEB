a12a8010dd3e19edea05838f592d06feaee541c9
KOlaGrek <kolagrek826@gmail.com>
2023-05-03 21:15:00 +0300

    LAB_6 add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
-import {Layout} from 'antd';
+import {Layout, Menu, MenuProps} from 'antd';
 import AppHeader from './component/AppHeader';
 import React, {useContext, useEffect} from "react";
 import MyFooter from "./component/Footer/MyFooter";
 import {useStore} from "./Store/UseStore";
+import {BrowserRouter, NavLink, Route, Router, Routes} from "react-router-dom";
+import Products from "./component/Products";
+import About from "./component/About";
 
 
+const {Header, Content} = Layout;
 
+const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
 const App = () => {
     const context = useStore()
-    useEffect(()=>{
+    useEffect(() => {
         try {
 
             context?.setItems()
-        }catch (e){
+        } catch (e) {
 
         }
         console.log(context)
-    },[])
+    }, [])
     return (
-        <Layout className="layout">
-            <AppHeader/>
+        <BrowserRouter>
+
+
+            <Layout className="layout">
+                {/*<Header className="header">*/}
+                {/*    <div className="logo"/>*/}
+                {/*    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1}/>*/}
+                {/*</Header>*/}
+                <div className="header">
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/products'>Products</NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                </div>
+
+                {/*<AppHeader/>*/}
 
 
 
-            <MyFooter/>
-        </Layout>
+                <Routes>
+                    <Route path='/' element={<AppHeader/>}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/about' element={<About/>}/>
+                </Routes>
+                <MyFooter/>
+            </Layout>
+        </BrowserRouter>
     );
 };
 
